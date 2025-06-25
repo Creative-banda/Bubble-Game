ec3def57b164f6384ddb7b1b9ab7cd1a0cdc078b
Creative-banda <4kpuspa@gmail.com>
2025-06-25 11:26:19 +0530

    first commit

---
+++ variations/1.1.py
@@ -0,0 +1,30 @@
+# 1.1 Create a Pygame window
+# This code will open a Pygame window with the dimensions 400x800 pixels and with the title "Bubble Dash".
+# We will understand clock, Its use and what is events in pygame and how to handle them.
+
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Game variables
+running = True
+
+# Screen dimensions
+WIDTH, HEIGHT = 400, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bubble Dash")
+
+# Clock for FPS control
+clock = pygame.time.Clock()
+
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()  # Update display
+    clock.tick(60)  # Limit FPS to 60
+
+pygame.quit()
---
+++ variations/1.2.py
@@ -0,0 +1,41 @@
+# 1.2 In this Code Snippet, We will Create a Rectangle in the window.
+# We will understand how to create a rectangle and it arguments.
+
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Game variables
+running = True
+
+# Screen dimensions
+WIDTH, HEIGHT = 400, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bubble Dash")
+
+# Clock for FPS control
+clock = pygame.time.Clock()
+
+
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Drawing a Rectangle (player)
+    pygame.draw.rect(screen, (0, 255, 0), (WIDTH // 2, HEIGHT - 100, 50, 70))
+
+    pygame.display.flip()  # Update display
+    clock.tick(60)  # Limit FPS to 60
+
+pygame.quit()
+
+
+# For Creating Rectangle you have to use pygame.draw.rect(screen, color, (x, y, width, height))
+# pygame.draw.rect() function parameters:
+# - screen: The surface on which to draw the rectangle.
+# - color: Color of the rectangle in RGB format
+# - (x, y): x and y position of the rectangle
+# - width and height: Width and height of the rectangle
